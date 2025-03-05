3cc049cc09983dd65803f626f18538c636d37b06
kirill010106 <konstruktor.png22@mail.ru>
2025-03-05 12:32:58 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module rest-api
+
+go 1.23.0
+
+toolchain go1.23.6
+
+require (
+	github.com/BurntSushi/toml v1.4.0
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.10.0
+)
+
+require golang.org/x/crypto v0.35.0
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lib/pq v1.10.9
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,36 @@
+package store_test
+
+import (
+	"rest-api/internal/app/model"
+	"rest-api/internal/app/store"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepository(t *testing.T) {
+	db, teardown := store.TestDB(t, databaseURL)
+	defer teardown("users")
+	s := store.New(db)
+	u, err := s.User().Create(model.TestUser(t))
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	db, teardown := store.TestDB(t, databaseURL)
+	defer teardown("users")
+	s := store.New(db)
+
+	email := "user@example.org"
+
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
+	u, err = s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
